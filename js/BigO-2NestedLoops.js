08550d43a7e7f257236d11c83ffcbba3a4e41f69
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-03-12 21:54:20 -0500

    initial devs

---
+++ js/BigO-2NestedLoops.js
@@ -0,0 +1,9 @@
+function logitems(n) {
+  for (let i = 0; i <= n; i++) {
+    for (let j = 0; j <= n; j++) {
+      console.log(i, j);
+    }
+  }
+}
+
+logitems(3);
--- js/BigO-TwoLoops.js
+++ js/BigO-TwoLoops.js
@@ -2,6 +2,9 @@ function logitems(n) {
   for (let i = 0; i <= n; i++) {
     console.log(i);
   }
+  for (let j = 0; j <= n; j++) {
+    console.log(j);
+  }
 }
 
-logitems(10);
+logitems(3);
